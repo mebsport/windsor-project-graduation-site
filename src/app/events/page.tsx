dec2e1193b723fdb4c75546935fa2aa563054037
mebsport <56307645+mebsport@users.noreply.github.com>
2024-03-12 08:30:00 -0400

    Update page.tsx

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,3 +1,4 @@
+import react from 'react'
 import NavBar from '../components/NavBar'
 import EventList from '../components/EventList'
 
@@ -6,14 +7,6 @@ export default function Home() {
     <main className='overflow-x-hidden flex min-h-screen flex-col bg-white'>
       <NavBar />
       <div className='container mt-24 mx-auto py-4 px-12'>
-        <EventList />
-        {/* <EventCalendar
-        events={[
-          {date: subDays(new Date(),6), title: "Post Video"},
-          {date: subDays(new Date(),1), title: "Edit Video"},
-          {date: subDays(new Date(),3), title: "Code"}
-        ]}
-        /> */}
         <h1>EVENTS</h1>
       </div>
     </main>

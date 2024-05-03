338b1d9ddcbe73332094e29b7bea68618e426478
mebsport <56307645+mebsport@users.noreply.github.com>
2024-05-03 09:47:23 -0400

    Create AboutSection.tsx

---
+++ src/app/components/AboutSection.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const AboutSection = () => {
+  return (
+    <div id='about'>
+      <p className='mt-6 text-black font-semibold text-base sm:text-lg lg:text-xl mb-6'>
+        Project Graduation is an annual event that is exclusively organized for
+        the graduating seniors of Windsor High School. The event is aimed at
+        providing a safe, drug-free, and fun-filled environment for the seniors
+        to celebrate their graduation with their friends. The event is entirely
+        managed and organized by parent volunteers, who put in a year-long
+        effort to raise funds and plan for the event.
+      </p>
+      <p className='text-black font-semibold text-base sm:text-lg lg:text-xl mb-6'>
+        The night is full of entertaining activities, including music and
+        dancing with a live DJ, delicious food, fun games, and exciting prizes.
+        The main objective of Project Graduation is to offer the seniors a
+        chance to celebrate their graduation in a secure and comfortable
+        environment, away from the potential dangers of drugs and alcohol.
+      </p>
+      <p className='text-black font-semibold text-base sm:text-lg lg:text-xl mb-6'>
+        This event is made possible through the generous donations of the
+        community members who believe in the importance of providing a safe and
+        memorable graduation experience for the seniors. The party starts at 11
+        PM and ends at 3 AM, and all the seniors are guaranteed to have a great
+        time, create everlasting memories, and stay safe throughout the night.
+      </p>
+    </div>
+  )
+}
+
+export default AboutSection
